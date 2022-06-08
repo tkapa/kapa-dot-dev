3849a86055e139a902fd85df812556d16e07da6c
Tylah Kapa <TylahKapa@gmail.com>
2022-06-08 15:57:24 +1000

    ✨Add markdown rendering

--- kapa_next/pages/post/[slug].js
+++ kapa_next/pages/post/[slug].js
@@ -1,13 +1,13 @@
 import Layout from "../../components/layout";
+import markdownToHtml from "../../lib/markdownToHtml";
 import { fetcher } from "../api/api";
 
-const Post = ({ post }) => {
+const Post = ({ post, content }) => {
     return(
         <Layout>
             <h1>{post.attributes.title}</h1>
-            <div>
-                {post.attributes.content}
-            </div>
+            <div classNmae='tracking-wide font-normal text-sm' 
+                dangerouslySetInnerHTML={{__html: content}}></div>
         </Layout>
     )
 }
@@ -17,12 +17,12 @@ export default Post;
 export async function getServerSideProps({ params }) {
     const { slug } = params;
     const postResponse = await fetcher(`${process.env.STRAPI_BASE_URI}/slugify/slugs/post/${slug}`);
-
-    console.log(postResponse);
+    const content = await markdownToHtml(postResponse.data.attributes.content);
 
     return{
         props: {
-            post: postResponse.data
+            post: postResponse.data,
+            content
         }
     }
 }

07024168ceb886f25657878530b3e6aedddc050d
Tylah Kapa <TylahKapa@gmail.com>
2023-02-17 10:28:56 +1000

    Update React + Fix Hydration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import React from "react";
 import "../styles.css";
 
 const App = ({ Component, pageProps }) => {
+  const [hasMounted, setHasMounted] = React.useState(false);
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
+
   return <Component {...pageProps} />;
 };
 
